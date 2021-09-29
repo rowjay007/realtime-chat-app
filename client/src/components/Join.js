b93aa2089ae32d7b94cee5276b3e0f574f111002
rowjay007 <rowlives@gmail.com>
2021-09-29 20:46:40 +0100

    creating join components input

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "react-router-dom";
 
 const Join = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
   return (
-    <div>
-      <h3>Hello from Join</h3>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="jointInput"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="jointInput mt-20"
+            type="text"
+            onChange={(e) => setRoom(e.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20">Sign In</button>
+        </Link>
+      </div>
     </div>
   );
 };
