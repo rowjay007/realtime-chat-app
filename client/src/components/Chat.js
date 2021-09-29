1b76c47077ccd30d5c6b8be9b5a574c7729fc221
rowjay007 <rowlives@gmail.com>
2021-09-29 19:44:36 +0100

    creating join and chat componetnts

---
+++ client/src/components/Chat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div>
+      <h3>Hello from client</h3>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ client/src/components/Join.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Join = () => {
+  return (
+    <div>
+      <h3>Hello from Join</h3>
+    </div>
+  );
+};
+
+export default Join;
