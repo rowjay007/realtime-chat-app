7ec9880a585d050a26904e98ce196ee58421260c
rowjay007 <rowlives@gmail.com>
2021-10-02 12:47:50 +0100

    import addUser to server with socket connection for the room and name

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const socketio = require("socket.io");
 const http = require("http");
 const router = require("./router");
 const cors = require("cors");
+const { addUser } = require("./users");
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,15 +15,17 @@ io.on("connection", (socket) => {
   console.log("We have a new connection"); 
 
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
-
-    // const error = true;
-
-    // if (error) {
-    //   callback({ error: "error" });
-    // }
+    const { error, user } = addUser({ id: socket.id, name, room })
+    
+    if (error) return callback(error)
+    socket.emit("message", { user: "admin", text: `${user.name}, welcome to the room ${user.room}` })
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined` })
+    
+    socket.join(user.room)
+    callback()
   });
 
+  
   socket.on("disconnect", () => {
     console.log("User had left!!!");
   });
