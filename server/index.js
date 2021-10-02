7ba3e52535c973566010b30c712db14169341516
rowjay007 <rowlives@gmail.com>
2021-10-02 11:52:37 +0100

    resolved cors error in server file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 const router = require("./router");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,9 +11,14 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("We have a new connection!!!");
-  socket.on("disconnection", () => {
-    console.log("User had left");
+  console.log("We have a new connection");
+
+  socket.on("join", ({ name, room }) => {
+    console.log(name, room);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User had left!!!");
   });
 });
 
