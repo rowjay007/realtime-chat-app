b4b478336fcc4e7df9cfc76b88e1fad42fd03079
rowjay007 <rowlives@gmail.com>
2021-10-02 14:28:05 +0100

    creating sendMessage callback

--- server/index.js
+++ server/index.js
@@ -12,20 +12,30 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("We have a new connection"); 
+  console.log("We have a new connection");
 
   socket.on("join", ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room })
-    
-    if (error) return callback(error)
-    socket.emit("message", { user: "admin", text: `${user.name}, welcome to the room ${user.room}` })
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined` })
-    
-    socket.join(user.room)
-    callback()
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined` });
+
+    socket.join(user.room);
+    callback();
+  });
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
   });
 
-  
   socket.on("disconnect", () => {
     console.log("User had left!!!");
   });
