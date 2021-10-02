d5a99032cec36d8f3c5aeaf4c0482d4b84349638
rowjay007 <rowlives@gmail.com>
2021-10-02 12:22:53 +0100

    creating users file in server folder

--- server/index.js
+++ server/index.js
@@ -11,10 +11,16 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("We have a new connection");
+  console.log("We have a new connection"); 
 
-  socket.on("join", ({ name, room }) => {
+  socket.on("join", ({ name, room }, callback) => {
     console.log(name, room);
+
+    // const error = true;
+
+    // if (error) {
+    //   callback({ error: "error" });
+    // }
   });
 
   socket.on("disconnect", () => {
